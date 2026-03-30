1687e9c6cfaf2e61743194d08691a77fa7c22d3b
Usanogh-5 <gayanehovhannisyan977@gmail.com>
2026-03-30 10:56:34 +0400

    Added class Shrjan with makeres and erkarutyun methods

---
+++ Shrjanagic.py
@@ -0,0 +1,8 @@
+import math
+class Shrjan:
+    def __init__(self, r):
+        self.r = r
+    def makeres(self):
+        return math.pi * self.r**2
+    def erkarutyun(self):
+        return 2 * math.pi * self.r
